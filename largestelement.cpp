4121a7c871cf714893d90d5f6acd5e0f689d8815
dharu58 <dharunikkakavitha@gmail.com>
2026-06-29 15:39:06 +0530

    Add files via upload

---
+++ largestelement.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int largestelement (vector<int> &arr, int n){
+    int largest = a[0];
+    for (int i = 0; i < n; i++){
+        if (arr[i] > largest){
+            largest = arr[i];
+
+        }
+        }
+        return largest;
+}
