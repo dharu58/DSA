3f37d9d09266abfbc7d271879bfe6d5626169715
dharu58 <dharunikkakavitha@gmail.com>
2026-07-20 16:53:06 +0530

    Add files via upload

---
+++ second_smallest.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main(){
+    int n; 
+    cout << "Enter the number of elements in the array : ";
+    cin >> n; 
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        cout << "Enter the element " << i << ": ";
+        cin >> arr[i];
+    }
+
+    int smallest = arr[0];
+    int ssmallest = INT_MAX;
+    for(int i = 0; i < n; i++){
+        if(arr[i] < smallest){
+            ssmallest = smallest;
+            smallest = arr[i];
+        }else if(arr[i] != smallest && arr[i] < ssmallest){
+            ssmallest = arr[i];
+        }
+
+    }
+    cout << smallest <<" "<<  ssmallest ; 
+    return 0;
+}
