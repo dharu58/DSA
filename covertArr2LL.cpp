335708a81535ec2aebf48f0272b0940b0292c2c8
dharu58 <dharunikkakavitha@gmail.com>
2026-09-08 16:32:12 +0400

    Add files via upload

---
+++ covertArr2LL.cpp
@@ -0,0 +1,25 @@
+class Node{
+    int data;
+    Node* next;
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head= new Node(arr[0]);
+    Node*mover = head;
+    for(int i = 1; i < arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp;
+    } 
+    return head;
+}
+
+int main(){
+    vector<int> arr = {12,5,8,8,7};
+    Node* head = convertArr2LL(arr);
+    cout << head->data;
+}
