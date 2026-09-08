cae6f00bf51377dc73ec743a3a3526669f066854
dharu58 <dharunikkakavitha@gmail.com>
2026-09-08 16:30:29 +0400

    Add files via upload

---
+++ removetailinLL.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+
+};
+
+Node* convertArr2LL(vector<int>&arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover -> next = temp;
+        mover = temp;
+    }
+    return head;
+}
