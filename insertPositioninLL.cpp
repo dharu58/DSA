d769fd0fa841cd281b1c098c34caa8f9bb0988be
dharu58 <dharunikkakavitha@gmail.com>
2026-09-10 15:36:32 +0400

    Add files via upload

---
+++ insertPositioninLL.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+
+};
+
+Node* convertArr2LL(vector<int>& arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for(int i = 1; i < arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover -> next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+void print(Node* head){
+    while(head != NULL){
+        cout << head->data<<" ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node* insertPosition(Node* head, int el, int k){
+    if(head == NULL){
+        if(k == 1){
+            return new Node(el);
+        }else{
+            return head;
+        }
+    }
+    if(k == 1){
+        return new Node(el, head);
+    }
+    int cnt = 0; 
+    Node* temp = head;
+    while(temp != NULL){
+        cnt++;
+        if(cnt == (k-1)){
+            Node* x = new Node(el,temp->next);
+            temp -> next = x;
+            break;
+        }
+        temp = temp -> next;
+    }
+    return head;
+}
+
+int main(){
+    vector<int> arr= {12, 5, 8, 7};
+    Node* head = convertArr2LL(arr);
+    head = insertPosition(head,100,1);
+    print(head);
+}
